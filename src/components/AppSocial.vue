4d42e377e8ee3e20742dc371949085d73b66c2ae
AndreaGul <andreagulino99@gmail.com>
2023-12-09 20:25:32 +0100

    feat:js socials

--- src/components/AppSocial.vue
+++ src/components/AppSocial.vue
@@ -1,15 +1,59 @@
 <script>
 export default {
     name : 'Social',
+    data(){
+        return {
+            category: 'Follow us',
+            Links: [
+            {
+                name: 'Facebook',
+                img: '/img/footer-facebook.png',
+                link: '#',
+                target: '_self',
+                
+            },
+            {
+                name: 'Twitter',
+                img: '/img/footer-twitter.png',
+                link: '#',
+                target: '_self',
+                
+            },
+            {   
+                name: 'Youtube',
+                img: '/img/footer-youtube.png',
+                link: '#',
+                target: '_self',
+                
+            },
+            {   
+                name: 'Pinterest',
+                img: '/img/footer-pinterest.png',
+                link: '#',
+                target: '_self',
+                
+            },
+            {
+                name: 'periscope',
+                img: '/img/footer-periscope.png',
+                link: '#',
+                target: '_self',
+                
+            },
+            ]
+        }
+    }
 }
 
 </script>
 
 <template>
     <ul>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
+        <li><h2>{{ category }}</h2></li>
+        <li v-for="link in Links">
+            <a :href="link.link" :target="link.target"><img :src="link.img" alt=""></a>
+            
+        </li>
     </ul>
 </template>
 

e07dba503931f57e977342ec9dd0212af4bea7db
AndreaGul <andreagulino99@gmail.com>
2023-12-09 20:08:08 +0100

    fix:struttura js

--- src/components/AppLinks.vue
+++ src/components/AppLinks.vue
@@ -3,70 +3,85 @@ export default {
     name : 'Links',
     data(){
         return{
-            category: 'Dc Comics',
-            navbarLinks: [
+            infoLinks: [
                 {
-                    name: 'Characters',
-                    link: '#',
-                    target: '_self',
-                    
+                    category: 'DC Comics',
+                    Links: [
+                    {
+                        name: 'Characters',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Comics',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Movies',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Tv',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Games',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Collectibles',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Videos',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Fans',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'News',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    {
+                        name: 'Shop',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    ]
                 },
                 {
-                    name: 'Comics',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Movies',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Tv',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Games',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Collectibles',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Videos',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Fans',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'News',
-                    link: '#',
-                    target: '_self',
-                    
-                },
-                {
-                    name: 'Shop',
-                    link: '#',
-                    target: '_self',
-                    
-                },
+                    category: 'Shop',
+                    Links: [
+                    {
+                        name: 'Shop DC',
+                        link: '#',
+                        target: '_self',
+                        
+                    },
+                    ]
+                }
             ]
-                
+           
         }
     }
 }
@@ -74,30 +89,15 @@ export default {
 </script>
 
 <template>
-    <ul>
-        <li><h2>{{ category }}</h2></li>
-        <li v-for="link in navbarLinks">
+    <ul v-for="info in infoLinks">
+        <li><h2>{{ info.category }}</h2></li>
+        <li v-for="link in info.Links">
             <a :href="link.link" :target="link.target">{{ link.name }}</a>
             
         </li>
         
     </ul>
-    <ul>
-        <li><h2>{{ category }}</h2></li>
-        <li v-for="link in navbarLinks">
-            <a :href="link.link" :target="link.target">{{ link.name }}</a>
-            
-        </li>
-        
-    </ul>
-    <ul>
-        <li><h2>{{ category }}</h2></li>
-        <li v-for="link in navbarLinks">
-            <a :href="link.link" :target="link.target">{{ link.name }}</a>
-            
-        </li>
-        
-    </ul>
+    
 </template>
 
 

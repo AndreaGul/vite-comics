e8afc702bc432f81220ad4ae93be432f40865f65
AndreaGul <andreagulino99@gmail.com>
2023-12-10 18:34:54 +0100

    feat:struttura main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,38 @@
 <script>
 export default {
     name : 'Main',
+    data (){
+        return{
+            text: '--> Contenti goes here <--',
+        }
+    }
 }
 
 </script>
 
 <template>
-    <header><p>Main</p></header>
+    <header >
+        <div class="container"> 
+            <p>{{text}}</p>
+        </div>
+    </header>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+
+
+header {
+    background-color: $primary-light-black;
+
+    .container {
+    padding: 56px 0;
+    p{
+        color: #ffffff;
+        font-size: 20px;
+    }
+    }
+}
+
+
 </style>
